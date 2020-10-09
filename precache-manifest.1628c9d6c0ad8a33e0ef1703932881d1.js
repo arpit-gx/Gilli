b5a6308433c9f1f5d56c65047117ca553a53893b
arpit-gx <arpityash36@gmail.com>
2020-10-09 13:44:10 +0530

    Updates

---
+++ precache-manifest.1628c9d6c0ad8a33e0ef1703932881d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb2eb892c2d48f03f89d1136400548c",
+    "url": "/gilli.github.io/index.html"
+  },
+  {
+    "revision": "18d345c385246f31836c",
+    "url": "/gilli.github.io/static/css/main.6c0d92e7.chunk.css"
+  },
+  {
+    "revision": "ab6a1a479ebe8cd31627",
+    "url": "/gilli.github.io/static/js/2.27a0a76b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/gilli.github.io/static/js/2.27a0a76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d345c385246f31836c",
+    "url": "/gilli.github.io/static/js/main.c64b35da.chunk.js"
+  },
+  {
+    "revision": "1b701591b8f533e9a64b",
+    "url": "/gilli.github.io/static/js/runtime-main.3d5cc527.js"
+  },
+  {
+    "revision": "61c2f99aa431dbbf602b3856a3fa463c",
+    "url": "/gilli.github.io/static/media/GLXX.61c2f99a.png"
+  },
+  {
+    "revision": "5ae71eeda879b29fa650c7639a1575de",
+    "url": "/gilli.github.io/static/media/VRS.5ae71eed.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gilli.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
