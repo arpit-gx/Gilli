03092428ff355bc1612ac723f7aa36b77434ae5f
arpit-gx <67437141+arpit-gx@users.noreply.github.com>
2024-12-04 23:02:22 +0530

    Update API.js

--- src/api/API.js
+++ src/api/API.js
@@ -1,12 +1,12 @@
 //const API_KEY = "";
 
 export const getMatches = () => {
-    const url= `https://cricapi.com/api/matches?apikey=JpOkehYBrFcbjfhcFLk4dGDCKar2`;
+    const url= `https://cricapi.com/api/matches?apikey=43c16216-2a52-4a3e-a792-b6d501a1b008`;
 
     return fetch(url).then((response) => response.json()).catch((error)=>console.log("ERROR:", error));
 }
 
 export const getMDetails=(id)=>{
-    const url = `https://cricapi.com/api/cricketScore?apikey=JpOkehYBrFcbjfhcFLk4dGDCKar2&unique_id=${id}`
+    const url = `https://cricapi.com/api/cricketScore?apikey=43c16216-2a52-4a3e-a792-b6d501a1b008&unique_id=${id}`
 return fetch(url).then((response) => response.json()).catch((error)=>console.log(error));
 }
